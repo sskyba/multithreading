e06b5e3f3e21b9f4424bcdb1ab9ea156005844c1
Sergey Skyba <sskyba@gmail.com>
2018-11-16 10:07:45 -0500

    Comments added

--- Multithread/ThreadController.java
+++ Multithread/ThreadController.java
@@ -2,43 +2,68 @@ package Multithread;
 
 import java.util.*;
 
+// Worker class implements a thread with prime numbers finding within the given range
 class Worker extends Thread {
   private String threadName = "";
+  private int startNumber;
+  private int endNumber;
+  private ArrayList<Integer> lResult = new ArrayList();
 
   Worker(String name) {
     threadName = name;
     System.out.println("Creating " + threadName );
   }
   
+  // private function to determine if the given number is prime
   private boolean isPrimeNumber(int n) {
-    for (int i=2; i<n/2; ++i) {
-      if (n % i == 0) return false;
-    }
+    for (int i=2; i<n/2; ++i) if (n % i == 0) return false;
     return true;
   }
   
-  public ArrayList<Integer> start(int nStart, int nEnd) {
-    ArrayList<Integer> lResult = new ArrayList();
-    for (int i=nStart; i<=nEnd; i++) {
+  // Thread body: the prime number finding
+  @Override
+  public void run() {
+    System.out.println("Running " + threadName );
+    for (int i=startNumber; i<=endNumber; i++) {
       if (isPrimeNumber(i)) {
         System.out.println("Thread " + this.getName() + " adding number " + i);
         lResult.add(i);
       }
     }
-    return lResult;
+   }
+  
+  // main method to initialize the thread and start running 
+  public ArrayList<Integer> start(int nStart, int nEnd) {
+    startNumber = nStart;
+    endNumber = nEnd;
+    run();
+    return this.lResult;
   }
 
 }
 
+// main class of the application
 public class ThreadController {
   
+  // max limit for the prime number findings
   static final int nMaxNumber = 1000000;
+  
+  // size of the chunk per thread
   static final int nStep = 100000;
+  
+  // pointer to the number of already processed numbers
   static int nNumberCount = 0;
+  
+  // max number of the threads allowed
   static final int nThreadMax = 3;
+  
+  // list of prime numbers (end result of the app)
   static ArrayList<Integer> lResult = new ArrayList();
+  
+  // list of threads
   static ArrayList<Worker> lThreads = new ArrayList();
 
+  // checks if all the threads are finished
   private static boolean allThreadsFinished() {
     for (int i=0; i<nThreadMax; i++) if (lThreads.get(i).isAlive()) return false;
     return true;
